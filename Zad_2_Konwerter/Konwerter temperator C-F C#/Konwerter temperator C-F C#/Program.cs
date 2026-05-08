c785ec904d0cb5200ba33527cda1e2e439e56afb
mdudek-bizness <mdudek.bizness@gmail.com>
2026-05-08 21:57:30 +0200

    Zadanie z temperaturą gotowe, można by dodać obsługę temperatur z wartościami dziesiętnymi

---
+++ Zad_2_Konwerter/Konwerter temperator C-F C#/Konwerter temperator C-F C#/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Konwerter_Temperatur
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Wybierz, na jakie jednostki chcesz przeliczyć temperaturę (C lub F): ");
+            string jednostka = Console.ReadLine().ToUpper(); //dajemy ToUpper gdyby użytkownik podał małą literę
+
+            Console.WriteLine("Podaj wartość temperatury: ");
+            var temperatura = int.Parse(Console.ReadLine());
+
+            //przeliczniki
+            var temp_F = temperatura * 1.8 + 32;
+            var temp_C = (temperatura - 32) / 1.8;
+            if (jednostka == "F"){
+                Console.WriteLine($"Temperatura w stopniach Celsjusza wynosi: {temp_C}");
+            }
+            else if (jednostka == "C")
+            {
+                Console.WriteLine($"Temperatura w stopniach Farenheita wynosi: {temp_F}");
+            }
+            else
+            {
+                Console.WriteLine("Nie rozpoznano jednostki docelowej. Użyj C lub F");
+            }
+        }
+    }
+}
